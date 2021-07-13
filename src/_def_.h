3699fa5c5409205dc71f97e7d4faaddf1008015b
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2021-07-13 14:05:59 +0300

    added release workflow

--- src/_def_.h
+++ src/_def_.h
@@ -15,6 +15,19 @@
 #define __ORM_BEGIN__ __MAIN_NAMESPACE_BEGIN__ namespace orm {
 #define __ORM_END__ } __MAIN_NAMESPACE_END__
 
+__ORM_BEGIN__
+
+namespace v
+{
+const uint major = 0;
+const uint minor = 0;
+const uint patch = 0;
+
+const std::string version = std::to_string(major) + "." + std::to_string(minor) + "." + std::to_string(patch);
+};
+
+__ORM_END__
+
 // xw::orm::abc
 #define __ORM_ABC_BEGIN__ __ORM_BEGIN__ namespace abc {
 #define __ORM_ABC_END__ } __ORM_END__
