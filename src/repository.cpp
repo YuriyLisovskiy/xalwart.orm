96e888c9cd4564d912f740c7f7fa72297abb92a3
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2021-09-29 16:38:53 +0300

    added Repository and Transaction helper classes

---
+++ src/repository.cpp
@@ -0,0 +1,20 @@
+/**
+ * repository.cpp
+ *
+ * Copyright (c) 2021 Yuriy Lisovskiy
+ */
+
+#include "./repository.h"
+
+// C++ libraries.
+// TODO
+
+// Orm libraries.
+// TODO
+
+
+__ORM_BEGIN__
+
+
+
+__ORM_END__
---
+++ src/transaction.cpp
@@ -0,0 +1,20 @@
+/**
+ * transaction.cpp
+ *
+ * Copyright (c) 2021 Yuriy Lisovskiy
+ */
+
+#include "./transaction.h"
+
+// C++ libraries.
+// TODO
+
+// Orm libraries.
+// TODO
+
+
+__ORM_BEGIN__
+
+
+
+__ORM_END__
